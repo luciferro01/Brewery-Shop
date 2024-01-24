d45e1722bc892a66da2688f3919f8abb2b218088
luciferro01 <lucifer1111ronaldo@gmail.com>
2024-01-24 18:40:46 +0530

    Customised ImageBakcgroundInfo Content

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {useRoute} from '@react-navigation/native';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation, route}: any) => {
+  // console.log(useRoute().params);
+  console.log(route.params);
   return (
     <View>
       <Text>DetailsScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DetailsScreen
+export default DetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
