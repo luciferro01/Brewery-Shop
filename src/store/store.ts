15586591b1b86c43477da85b2396cde0fa1a6713
luciferro01 <lucifer1111ronaldo@gmail.com>
2024-01-24 15:48:23 +0530

    Add to Cart and CalculatePrice methods in Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,16 +3,73 @@ import {persist, createJSONStorage} from 'zustand/middleware';
 import CoffeeData from '../data/CoffeeData';
 import BeansData from '../data/BeansData';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {produce} from 'immer';
+import {ToastAndroid} from 'react-native';
 
 export const useStore = create(
   persist(
-    (get, set) => ({
+    (set, get) => ({
       CoffeeList: CoffeeData,
       BeansList: BeansData,
       CartPrice: 0,
       FavoritesList: [],
       CartList: [],
       OrderHistoryList: [],
+
+      addToCart: (cartItem: any) =>
+        set(
+          produce(state => {
+            let found = false;
+            for (let i = 0; i < state.CartList.length; i++) {
+              if (state.CartList[i].id == cartItem.id) {
+                found = true;
+                let size = false;
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (
+                    state.CartList[i].prices[j].size == cartItem.prices[0].size
+                  ) {
+                    size = true;
+                    state.CartList[i].prices[j].quantity++;
+                    break;
+                  }
+                }
+                if (size == false) {
+                  state.CartList[i].prices.push(cartItem.prices[0]);
+                }
+                state.CartList[i].prices.sort((a: any, b: any) => {
+                  if (a.size > b.size) {
+                    return -1;
+                  }
+                  if (a.size < b.size) {
+                    return 1;
+                  }
+                  return 0;
+                });
+                break;
+              }
+            }
+            if (found == false) {
+              state.CartList.push(cartItem);
+            }
+          }),
+        ),
+
+      CalculateCartPrice: () =>
+        set(
+          produce(state => {
+            let totalPrice = 0;
+            for (let i = 0; i < state.CartList.length; i++) {
+              let tempPrice = 0;
+              for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                tempPrice =
+                  parseFloat(state.CartList[i].prices[j].price) *
+                  state.CartList[i].prices[j].quantity;
+              }
+              state.CartList[i].ItemPrice = tempPrice.toFixed(2).toString;
+              totalPrice = tempPrice + totalPrice;
+            }
+          }),
+        ),
     }),
     {
       name: 'coffee-app',
